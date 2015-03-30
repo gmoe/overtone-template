6fd4c85a0fc3c5b8c31d644546768c29348331be
Griffin Moe <me@griffinmoe.com>
2015-03-29 19:32:22 -0500

    Fixed the project definition.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject overtone/lein-template "1.0.0"
+(defproject org.clojars.gmoe/overtone-template "1.0.0"
   :description "A simple Leiningen template for Overtone."
   :url "http://example.com/FIXME"
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo
             :comments "Please use Overtone for good"}
-  :dependencies [[overtone "0.9.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"] 
+                 [overtone "0.9.1"]]
   :eval-in-leiningen true)
